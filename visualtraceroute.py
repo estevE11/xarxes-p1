7f0a9dd53b2a95d252380dddf86291d9a10b6092
ezixd23 <ziad.elkarrabi@gmail.com>
2023-10-02 22:39:52 +0200

    enviar paquete y calcular rtt

--- visualtraceroute.py
+++ visualtraceroute.py
@@ -2,16 +2,40 @@ import socket
 import time
 import ipinfo
 import matplotlib.pyplot as plt
-from scapy.all import *
+from scapy.all import Conf, IP, ICMP, sr1
 
 
-def get_ip_info():
+def get_ip_info(ip):
     access_token = '92ea3b28d5471d'
     handler = ipinfo.getHandler(access_token)
-    details = handler.getDetails()
+    details = handler.getDetails(ip)
     return details
 
-def traceroute(dest_name) :
+def traceroute(dest_addr) :
     Conf.route.add(net="0.0.0.0/0", dev="ens33")
 
-    dest_addr = socket.gethostbyname(dest_name)
+    dest_name = socket.gethostbyaddr(dest_addr)
+    max_ttl = 30
+    ttl = 1
+
+    while True:
+        time_sent = time.time()
+
+        package_icmp = IP(dst=dest_addr,ttl=ttl) / ICMP()
+        response = sr1(package_icmp,verbose=False)
+
+        time_received = time.time()
+
+        rtt_mls = (time_received - time_sent) * 1000
+
+        if response:
+            ip = response.src
+            #esto lo usaremos luego cuando decubramos como dibujar con el mapa
+            details_ip = get_ip_info(ip)
+            
+            print(f"RTT al host='{dest_name}' ({ip}) es de {rtt_mls:.2f} ms")
+            
+            if response.src == dest_addr:
+                break
+        else:
+            print(f"* * * * * *")
